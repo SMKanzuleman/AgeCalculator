33714d7ba17492231f27a07b13e12c1b0083a662
SMKanzulEman <syedkanzuleman.official@gmail.com>
2026-03-09 07:24:42 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
+    //Nothing
+
     <div className='bg-blue-400 w-screen h-screen flex justify-center items-center'>
       <h1 className='text-9xl font-bold '>Nothing</h1>
       <input type="date" className='px-5 py-8 border w-60' />
@@ -11,3 +13,7 @@ const App = () => {
 }
 
 export default App
+
+
+
+
