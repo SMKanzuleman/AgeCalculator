2e774e696713e0440da1397cc0e8572fdffd6352
SMKanzulEman <syedkanzuleman.official@gmail.com>
2026-03-11 17:21:20 +0500

    Made Core Age calculate Function in JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-
+import CalculateAge from './utils/CalculateAge'
 const App = () => {
   return (
-    //Nothing
-
-    <div className='bg-blue-400 w-screen h-screen flex justify-center items-center'>
-      <h1 className='text-9xl font-bold '>Nothing</h1>
-      <input type="date" className='px-5 py-8 border w-60' />
-      
-    </div>
+    <h1>KanzulEman</h1>
   )
 }
 
---
+++ src/utils/CalculateAge.js
@@ -0,0 +1,25 @@
+function CalculateAge(inputdob) {
+  const today = new Date();
+  const dob = new Date(inputdob);
+
+  let years = today.getFullYear() - dob.getFullYear();
+  let months = today.getMonth() - dob.getMonth();
+  let days = today.getDate() - dob.getDate();
+
+  if (years < 0) {
+    return null;
+  }
+  //! If days are negative borrow one month(30 days) from months
+  if (days < 0) {
+    months--;
+    const PrevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    days += PrevMonth.getDate();
+  }
+  //! If months are negative borrow one year(12 months) from years
+  if (months < 0) {
+    years--;
+    months += 12;
+  }
+  return { years, months, days };
+}
+export default CalculateAge;
